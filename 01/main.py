eb1ec1d677c3b9db2ac4e5cc23c737cba9e1a8a7
klxcoder <klxcoder@gmail.com>
2025-04-03 21:25:19 +0700

    get_weighted_gini_impurity

--- 01/main.py
+++ 01/main.py
@@ -25,6 +25,14 @@ def get_left_right(train_data: list[tuple[str, int, str]], split: Callable[[tupl
             right.append(row)
     return left, right
 
+def get_weighted_gini_impurity(left: list[tuple[str, int, str]], right: list[tuple[str, int, str]]) -> float:
+    len_left = len(left)
+    len_right = len(right)
+    len_total = len_left + len_right
+    weighted_left = len_left / len_total * get_gini_impurity(get_labels(left))
+    weighted_right = len_right / len_total * get_gini_impurity(get_labels(right))
+    return weighted_left + weighted_right
+
 def main():
     train_data: list[tuple[str, int, str]] = [
         ('Green', 3, 'Apple'),
@@ -36,8 +44,8 @@ def main():
     gini_impurity = get_gini_impurity(get_labels(train_data))
     print(gini_impurity) # 0.6399999999999999
     left, right = get_left_right(train_data, lambda row: row[0] == "Green")
-    print(left) # [('Green', 3, 'Apple')]
-    print(right) # [('Yellow', 3, 'Apple'), ('Red', 1, 'Grape'), ('Red', 1, 'Grape'), ('Yellow', 3, 'Lemon')]
+    weighted_gini_impurity = get_weighted_gini_impurity(left, right)
+    print(weighted_gini_impurity) # 0.5
 
 if __name__ == "__main__":
     main()
